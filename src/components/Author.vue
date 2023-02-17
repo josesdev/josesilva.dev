a2c49fb3ba7571557731fc505384ac3df1df5580
José Silva <jose@lunite.net>
2023-02-17 00:07:08 -0300

    Update links

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -12,10 +12,10 @@
 		</p>
 
 		<p class="author__links">
-			<a href="https://twitter.com/jokerwin_" title="Follow me on Twitter" target="_blank" rel="nofollow noopener">
+			<a href="https://twitter.com/josesilvadev" title="Follow me on Twitter" target="_blank" rel="nofollow noopener">
 				<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
 			</a>
-			<a href="https://github.com/Jokerwin" title="Follow me on Github" target="_blank" rel="nofollow noopener">
+			<a href="https://github.com/josesdev" title="Follow me on Github" target="_blank" rel="nofollow noopener">
 				<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
 			</a>
 			<a href="https://www.instagram.com/jokerwin09/" title="Follow me on Instagram" target="_blank" rel="nofollow noopener">
@@ -24,7 +24,7 @@
 			<a href="https://t.me/jokerwin09" title="Say hello on Telegram" target="_blank" rel="nofollow noopener">
 				<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>
 			</a>
-			<a href="https://www.linkedin.com/in/jokerwin/" title="I'm on LinkedIn" target="_blank" rel="nofollow noopener">
+			<a href="https://www.linkedin.com/in/josesdev/" title="I'm on LinkedIn" target="_blank" rel="nofollow noopener">
 				<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
 			</a>
 			<a href="mailto:jose@lunite.net" title="Say hello with an email" target="_blank" rel="nofollow noopener">
